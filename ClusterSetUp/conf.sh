32386e099b971d32f7ce2a79cf55c66c06c20b03
wenqing.he <wenqinghe1016@gmail.com>
2024-10-29 20:37:50 +0800

    feat: minor change
    Signed-off-by: wenqing.he <wenqinghe1016@gmail.com>

--- ClusterSetUp/conf.sh
+++ ClusterSetUp/conf.sh
@@ -1,7 +1,7 @@
 echo "Setting up the environment for the cluster"
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl -w user.max_user_namespaces=28633
-sudo sysctl -w net.ipv4.conf.all.rp_filter=0
+sudo sysctl -w user.max_user_namespaces=126655
+sudo sysctl -w net.ipv4.conf.all.rp_filter=1
 apt install -y ipset ipvsadm
 sudo modprobe br_netfilter
 sudo modprobe nf_conntrack
